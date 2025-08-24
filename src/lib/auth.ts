904710fea5f77822327b998670675816ba3b00dd
MrMegaMango <amangocoding@gmail.com>
2025-08-23 21:52:27 -0700

    Simplify auth to use only Google OAuth provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,23 +1,14 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import GoogleProvider from "next-auth/providers/google"
-import GitHubProvider from "next-auth/providers/github"
 import { prisma } from "./prisma"
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    ...(process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET
-      ? [GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })]
-      : []),
-    ...(process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET
-      ? [GitHubProvider({
-          clientId: process.env.GITHUB_CLIENT_ID,
-          clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        })]
-      : []),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
   ],
   callbacks: {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -28,8 +19,5 @@ export const authOptions = {
         id: user.id,
       },
     }),
-  },
-  pages: {
-    signIn: '/auth/signin',
   },
 }
